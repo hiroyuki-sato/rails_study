e4340e58018e93db25f5d944db57d2b6531b6971
Hiroyuki Sato <hiroysato@gmail.com>
2016-04-04 21:18:33 +0900

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#
+# 名前,ふりがな,アドレス,性別,年齢,誕生日
+
+require 'csv'
+require 'pp'
+HEADER = %w[ name kana_name mail_address sex age birthday ]
+file = File.expand_path("../../private/users.csv",__FILE__)
+CSV.open(file).each_with_index do |user,idx|
+  next if idx == 0
+  User.create!(Hash[HEADER.zip(user.to_a)])
+end
